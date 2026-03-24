938ead750b9bcd835bf39a300a15aa7289a3f525
justinguto <jutogustowski@gmail.com>
2026-03-23 18:14:43 -0700

    finished

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
-// Rewrite the following program. Use a switch statement instead of the if/else if statement.
-
 #include <iostream>
 using namespace std;
+
 int main()
 {
-       int selection;
-        cout << "Which formula do you want to see?\n\n";
-        cout << "1. Apple\n";
-        cout << "2. Bannana\n";
-        cout << "3. Orange\n" ;
-        cout << "4. None of them\n";
-        cin >> selection;
-// TODO
-// TODO
+    int selection;
+
+    cout << "Which formula do you want to see?\n\n";
+    cout << "1. Apple\n";
+    cout << "2. Bannana\n";
+    cout << "3. Orange\n";
+    cout << "4. None of them\n";
+    cin >> selection;
+
+    switch (selection)
+    {
+        case 1:
+            cout << "Menu 1 selected";
+            break;
+        case 2:
+            cout << "Menu 2 selected";
+            break;
+        case 3:
+            cout << "Menu 3 selected";
+            break;
+        case 4:
+            cout << "Menu 4 selected";
+            break;
+        default:
+            cout << "Wrong menu item";
+            break;
+    }
+
+    return 0;
 }
